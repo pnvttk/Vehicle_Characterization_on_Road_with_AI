969baaee0fb9544372352a095cffe2004d5c7dc1
Pannavach T. <40768385+pnvttk@users.noreply.github.com>
2022-10-08 15:44:37 +0700

    update: easyocr, featherlight integrate

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from email.mime import image
 from enum import unique
+from operator import ne
+from tkinter import BOTTOM
 from tkinter.tix import Tree
 from flask import Flask, render_template, request, jsonify, redirect, url_for, flash
 from turtle import title
@@ -30,6 +32,8 @@ UPLOAD_FOLDER = './results'
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
 
 # upload folder
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -43,6 +47,11 @@ conn = pymysql.connect(host='localhost',
 # ? Local custom model
 model = torch.hub.load('./yolov5', 'custom', source='local',
                        path='./models_train/yolov5x.pt', force_reload=True)  # The repo is stored locally
+
+# ? all classname
+classes = model.names  # class names in string format
+# print(classes[0])
+# quit()
 
 # ? confidence threshold
 model.conf = 0.8
@@ -58,18 +67,21 @@ model.conf = 0.8
 
 
 # # ? datatables
-# @app.route("/dttb")
-# def dttb():
-#     cur = conn.cursor()
-#     cur.execute('select * from detect_data')
-#     tb_detect_data = cur.fetchall()
-#     return render_template('dttb.html', title='datatables', data=tb_detect_data)
+@app.route("/dttb")
+def dttb():
+    cur = conn.cursor()
+    cur.execute('select * from detect_data')
+    tb_detect_data = cur.fetchall()
+    return render_template('dttb.html', title='datatables', data=tb_detect_data)
 # img_base64 = ""
 
 # ? Unique name generate
+
+
 def generate_custom_name(original_file_name):
     unique_name = uuid.uuid4().hex
-    return unique_name + pathlib.Path(original_file_name).suffix
+    return unique_name
+    # return unique_name + pathlib.Path(original_file_name).suffix
 
 
 # ? Main Yolov5 Object Detection
@@ -90,7 +102,79 @@ def mask_image():
     # results.save(save_dir="results/" + new_name)
     # results.save(save_dir="results/test.jpg")
 
-    # quit()
+    # ? Count class number
+    results.pandas().xyxy[0]  # Pandas DataFrame
+    print("[INFO] : Pandas Results")
+    print(results.pandas().xyxy[0])
+    print("[INFO] : Count class name")
+    # results.pandas().xyxy[0].value_counts('name')  # class counts (pandas)
+    print(results.pandas().xyxy[0].value_counts(
+        'name'))  # class counts (pandas)
+
+    # ? Get access to class name
+    # ? info for array of results
+    info = results.pandas().xyxy[0].to_dict(orient='records')
+    # ? Loop to get only class name
+    i = 0
+    if len(info) != 0:
+        # print("[INFO] : results name")
+        # print(info)
+        for result in info:
+            getClass_name = result['name']
+            # left = result['xmin']
+            # top = result['ymin']
+            # right = result['xmax']
+            # bottom = result['ymax']
+            # # print(getClass_name)
+            # print('[LEFT] = ', left)
+            # print('[top] = ', top)
+            # print('[right] = ', right)
+            # print('[bottom] = ', bottom)
+
+            # crop
+            if getClass_name == 'Plate':
+                print('[INFO] Croping image.')
+                # crops = results.crop((left, top, right, bottom))
+                # crops.show()
+                # crops = results.crop(save=True)
+                results.crop(save_dir="results/" + new_name)
+                image_temp_path = "/results/" + new_name + "/image0.jpg"
+                crop_path = new_name + '/crops/Plate/'
+
+                for f in os.listdir("./results/"):
+                    print("[TEMP PATH] = "+f)
+                    # img_ocr = open('/' + crop_path + 'image' + str(i) + '.jpg')
+
+                img_ocr = str("./results/" + crop_path +
+                              'image' + str(i) + '.jpg')
+                print("[INFO] path for ocr")
+                print(img_ocr)
+                # os.path.exists(img_ocr)
+                # os.path.isfile(img_ocr)
+                # os.listdir(img_ocr)
+                eOCR = EASY_OCR.readtext(str(img_ocr), detail=0)
+                print(eOCR)
+                i = int(i)+1
+                print(i)
+            if i == 1:
+                i += 1
+                print(i)
+                i = str(i).zfill(2)
+                print(str(i))
+            # break
+    else:
+        image_temp_path = ""
+        return jsonify({'alert': "alert('error')"})
+
+    # img_ocr.show()
+
+    # # ? Crop results
+    # # results = model(img)  # inference
+    # if getClass_name == 'Plate':
+    #     print('[INFO] Croping image.')
+    #     crops = results.crop(save=True)
+    # # crops.save(save_dir="results/")
+    # # quit()
 
     # results.save(save_dir="results/")
     # return redirect("results/image0.jpg")
@@ -109,13 +193,13 @@ def mask_image():
             'utf-8'))  # base64 encoded image with results
 
     # ? Send to console
-    return jsonify({'status': str(b64str)})
+    return jsonify({'status': str(b64str), 'temp_image': str(image_temp_path)})
 
 
 # ? Set Header
-@app.after_request
+@ app.after_request
 def after_request(response):
-    print("log: setting cors", file=sys.stderr)
+    # print("log: setting cors", file=sys.stderr)
     response.headers.add('Access-Control-Allow-Origin', '*')
     response.headers.add('Access-Control-Allow-Headers',
                          'Content-Type,Authorization')
@@ -123,7 +207,7 @@ def after_request(response):
     return response
 
 
-@app.route("/", methods=['GET', 'POST'])
+@ app.route("/", methods=['GET', 'POST'])
 def new():
     return render_template('new.html')
 
@@ -139,8 +223,8 @@ if __name__ == "__main__":
     # model = torch.hub.load('./', 'yolov5x_model', pretrained=True)
 
     # ? local path
-    model = torch.hub.load('./yolov5', 'custom', source='local',
-                           path='./models_train/yolov5x.pt', force_reload=True)  # The repo is stored locally
+    # model = torch.hub.load('./yolov5', 'custom', source='local',
+    #                        path='./models_train/yolov5x.pt', force_reload=True)  # The repo is stored locally
 
-    model.eval()
+    # model.eval()
     app.run(host="0.0.0.0", port=args.port, debug=True)
