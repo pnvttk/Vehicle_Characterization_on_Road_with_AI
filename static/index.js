ef791e17904f417c0f3cd92b17ba61c2e62c0491
pnvttk <pannavach@outlook.com>
2022-10-08 15:44:37 +0700

    update: easyocr, featherlight integrate

--- static/index.js
+++ static/index.js
@@ -1,6 +1,7 @@
 window.onload = () => {
     $('#sendbutton').click(() => {
         imagebox = $('#imagebox')
+        abox = $('#abox')
         input = $('#imageinput')[0]
         if (input.files && input.files[0]) {
             let formData = new FormData();
@@ -19,9 +20,20 @@ window.onload = () => {
                 success: function (data) {
                     console.log("upload success");
                     console.log(data);
+                    a_msg = data['alert']
                     bytestring = data['status']
-                    // console.log("image :", bytestring)
-                    imagebox.attr('src', 'data:image/jpeg;base64,' + bytestring)
+                    // console.log("a_msg : ", a_msg)
+                    // console.log("bytestring : ", bytestring)
+                    if (bytestring != undefined) {
+                        temp_image_path = data['temp_image']
+                        console.log("temp_image_path :", temp_image_path)
+                        abox.attr('href', 'data:image/jpeg;base64,' + bytestring)
+                        imagebox.attr('src', 'data:image/jpeg;base64,' + bytestring)
+                        alert('Object detection success.')
+                    }
+                    if (a_msg != undefined) {
+                        alert("Error : Cannot detect Plate or Brand from this image.")
+                    }
                 }
             });
         }
@@ -32,6 +44,7 @@ window.onload = () => {
 
 function readUrl(input) {
     imagebox = $('#imagebox')
+    abox = $('#abox')
     console.log("evoked readUrl")
     if (input.files && input.files[0]) {
         let reader = new FileReader();
@@ -39,8 +52,9 @@ function readUrl(input) {
             console.log(e)
 
             imagebox.attr('src', e.target.result);
-            imagebox.height(500);
-            imagebox.width(800);
+            abox.attr('href', e.target.result)
+            // imagebox.height(500);
+            // imagebox.width(800);
         }
         reader.readAsDataURL(input.files[0]);
     }
