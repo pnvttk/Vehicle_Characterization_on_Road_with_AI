54745a495090714161608165f2d5f3320c1d9a6f
Pannavach T. <40768385+pnvttk@users.noreply.github.com>
2022-10-09 15:02:24 +0700

    update: better comment

--- static/index.js
+++ static/index.js
@@ -1,13 +1,22 @@
 window.onload = () => {
     $('#sendbutton').click(() => {
+        // ? html element
         imagebox = $('#imagebox')
         abox = $('#abox')
         plateBox = $('#plateBox')
         provinceBox = $('#provinceBox')
+
+        // ? Get image input
         input = $('#imageinput')[0]
+
+        // ? if get input
         if (input.files && input.files[0]) {
+
+            // ? payload to backend
             let formData = new FormData();
             formData.append('image', input.files[0]);
+
+            // ? ajax send to backend
             $.ajax({
                 url: "http://localhost:5000/detectObject",
                 type: "POST",
@@ -15,44 +24,72 @@ window.onload = () => {
                 cache: false,
                 processData: false,
                 contentType: false,
+
+                // ? error handle
                 error: function (data) {
                     console.log("upload error", data);
                     console.log(data.getAllResponseHeaders());
                 },
+
+                // ? handle return from backend
                 success: function (data) {
-                    console.log("upload success");
+
+                    // * check 
+                    // console.log("upload success");
                     // console.log(data);
+
                     a_msg = data['alert']
                     bytestring = data['status']
                     ocr_txt = data['json_ocr_txt']
+
+                    // * check
                     // console.log("a_msg : ", a_msg)
                     // console.log("bytestring : ", bytestring)
                     // console.log("ocr_txt : ", ocr_txt)
+
+                    // ? if get image
                     if (bytestring != undefined) {
-                        // temp_image_path = data['temp_image']
+
+                        // * check
                         // console.log("temp_image_path :", temp_image_path)
+
+                        // ? replace image with new image from backend
                         abox.attr('href', 'data:image/jpeg;base64,' + bytestring)
                         imagebox.attr('src', 'data:image/jpeg;base64,' + bytestring)
-                        // alert('Object detection success.')
+
+                        // ? alert msg
                         Swal.fire({
                             icon: 'success',
                             title: 'Successfully',
                             text: 'Obejct Detection have been compleate!',
                         })
                     }
+
+                    // ? if get ocr text
                     if (ocr_txt != undefined) {
+
+                        // ? empty array 
                         plate_arr = []
                         province_arr = []
+
+                        // ? loop to spilt key 
                         ocr_txt.forEach(plate => {
                             for (let key in plate) {
+
+                                // * check
                                 // console.log("For key in plate :")
                                 // console.log(`${key}: ${plate[key]}`)
 
+                                // ? get value
                                 ext_txt = `${plate[key]}`
+                                // ? remove special character
                                 remove_special_char = ext_txt.replace(/[&\/\\#,+()$~%.'":*?<>{}]/g, '');
+
+                                // * test
                                 // console.log(remove_special_char)
                                 // new_arr.push(remove_special_char)
 
+                                // ? spilt and push to new arr
                                 if (`${key}` == "plate") {
                                     // console.log("For key in plate :")
                                     // console.log(`${key}: ${plate[key]}`)
@@ -65,20 +102,27 @@ window.onload = () => {
                                 }
                             }
                         })
+
+                        // ? remove "," and create new line
                         plate_result = plate_arr.toString().split(",").join("\n")
                         province_result = province_arr.toString().split(",").join("\n")
+
+                        // * check
                         // console.log(result_arr)
+
+                        // ? set value in frontend
                         plateBox.val(plate_result)
                         provinceBox.val(province_result)
-                        // text_a.val(JSON.stringify(ocr_txt))
                     }
+
+                    // ? if alert msg is not set
                     if (a_msg != undefined) {
+                        // ? alert err msg
                         Swal.fire({
                             icon: 'error',
                             title: 'Oops...',
                             text: "Object Detection have been failed! \n Look like there isn't Plate or Brand in this image",
                         })
-                        // alert("Error : Cannot detect Plate or Brand from this image.")
                     }
                 }
             });
@@ -89,14 +133,24 @@ window.onload = () => {
 
 
 function readUrl(input) {
+    // ? html elment
     imagebox = $('#imagebox')
     abox = $('#abox')
-    console.log("evoked readUrl")
+
+    // * check
+    // console.log("evoked readUrl")
+
+    // ? if get image input
     if (input.files && input.files[0]) {
+
+        // ? read file
         let reader = new FileReader();
         reader.onload = function (e) {
+
+            // * log event
             // console.log(e)
 
+            // ? image preview
             imagebox.attr('src', e.target.result);
             abox.attr('href', e.target.result)
             // imagebox.height(500);
